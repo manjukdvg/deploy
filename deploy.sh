4a240e663282635c4147513be9f89f8604fc8adc
manjukdvg <manjukdvg@gmail.com>
2013-05-17 15:44:46 +0530

    latest

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ git checkout $BRANCH_NAME
 git pull origin $BRANCH_NAME
 cd ..
 
-spree_dir= ( glue spree-affiliate spree-store-credits spree_auth_devise spree_active_sale spree_ajax_add_to_cart spree_address_book spree_sunspot spree_size_chart spree_$
+pree_dir=(glue spree-affiliate spree-store-credits spree_auth_devise spree_active_sale spree_ajax_add_to_cart spree_address_book spree_sunspot spree_size_chart spree_taxon_promo spree_pos spree_static_content spree_variant_options spree_wishlist spree_social spree_ebsin spree_related_products spree_store_credits spree_editor spree_minicart spree_styletag_classic)
+
 
 for i in "${spree_dir[@]}"
 do
