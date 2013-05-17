76347e527193c8cd4308dc266f73bd292a2bf77a
Manjunath Bavihal <manjukdvg@gmail.com>
2013-05-17 13:08:50 +0530

    Update deploy.sh
    updated

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 BRANCH_NAME=master
-cd $HOME/apps/st
+cd apps/st
 git checkout $BRANCH_NAME
 git pull origin $BRANCH_NAME
+cd ..
+
+spree_dir= ( glue spree-affiliate spree-store-credits spree_auth_devise spree_active_sale spree_ajax_add_to_cart spree_address_book spree_sunspot spree_size_chart spree_$
+
+for i in "${spree_dir[@]}"
+do
+        echo $i
+        cd $i
+        git checkout $i
+        git pull origin $i
+        cd ..
+done
 
 #touch tmp/restart.txt
+
+
 
